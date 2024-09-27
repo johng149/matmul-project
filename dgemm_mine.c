e8536e8c94223f94a2b61c9ea0ddca0f5ee1bac0
johng149 <johnguo149@gmail.com>
2024-09-27 15:15:15 +0000

    Added kernels etc from test for 8x8

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -51,45 +51,45 @@ void kernel_4x4_4x4(const int Ma, const int Mb, const int Mc, double *restrict a
 
 void kernel_8x8_8x8(const int Ma, const int Mb, const int Mc, double *restrict a, double *restrict b, double *c)
 {
-    __m256d c0 = _mm256_loadu_pd(&C(0, 0));
-    __m256d c1 = _mm256_loadu_pd(&C(1, 0));
-    __m256d c2 = _mm256_loadu_pd(&C(2, 0));
-    __m256d c3 = _mm256_loadu_pd(&C(3, 0));
-    __m256d c4 = _mm256_loadu_pd(&C(4, 0));
-    __m256d c5 = _mm256_loadu_pd(&C(5, 0));
-    __m256d c6 = _mm256_loadu_pd(&C(6, 0));
-    __m256d c7 = _mm256_loadu_pd(&C(7, 0));
+    __m512d c0 = _mm512_loadu_pd(&C(0, 0));
+    __m512d c1 = _mm512_loadu_pd(&C(1, 0));
+    __m512d c2 = _mm512_loadu_pd(&C(2, 0));
+    __m512d c3 = _mm512_loadu_pd(&C(3, 0));
+    __m512d c4 = _mm512_loadu_pd(&C(4, 0));
+    __m512d c5 = _mm512_loadu_pd(&C(5, 0));
+    __m512d c6 = _mm512_loadu_pd(&C(6, 0));
+    __m512d c7 = _mm512_loadu_pd(&C(7, 0));
 
     for (int i = 0; i < 8; ++i)
     {
-        __m256d a0 = _mm256_set1_pd(AT(0, i));
-        __m256d a1 = _mm256_set1_pd(AT(1, i));
-        __m256d a2 = _mm256_set1_pd(AT(2, i));
-        __m256d a3 = _mm256_set1_pd(AT(3, i));
-        __m256d a4 = _mm256_set1_pd(AT(4, i));
-        __m256d a5 = _mm256_set1_pd(AT(5, i));
-        __m256d a6 = _mm256_set1_pd(AT(6, i));
-        __m256d a7 = _mm256_set1_pd(AT(7, i));
+        __m512d a0 = _mm512_set1_pd(A(0, i));
+        __m512d a1 = _mm512_set1_pd(A(1, i));
+        __m512d a2 = _mm512_set1_pd(A(2, i));
+        __m512d a3 = _mm512_set1_pd(A(3, i));
+        __m512d a4 = _mm512_set1_pd(A(4, i));
+        __m512d a5 = _mm512_set1_pd(A(5, i));
+        __m512d a6 = _mm512_set1_pd(A(6, i));
+        __m512d a7 = _mm512_set1_pd(A(7, i));
+        __m512d b0 = _mm512_loadu_pd(&B(i, 0));
 
-        __m256d b0 = _mm256_loadu_pd(&B(i, 0));
-
-        c0 = _mm256_fmadd_pd(a0, b0, c0);
-        c1 = _mm256_fmadd_pd(a1, b0, c1);
-        c2 = _mm256_fmadd_pd(a2, b0, c2);
-        c3 = _mm256_fmadd_pd(a3, b0, c3);
-        c4 = _mm256_fmadd_pd(a4, b0, c4);
-        c5 = _mm256_fmadd_pd(a5, b0, c5);
-        c6 = _mm256_fmadd_pd(a6, b0, c6);
-        c7 = _mm256_fmadd_pd(a7, b0, c7);
+        c0 = _mm512_fmadd_pd(a0, b0, c0);
+        c1 = _mm512_fmadd_pd(a1, b0, c1);
+        c2 = _mm512_fmadd_pd(a2, b0, c2);
+        c3 = _mm512_fmadd_pd(a3, b0, c3);
+        c4 = _mm512_fmadd_pd(a4, b0, c4);
+        c5 = _mm512_fmadd_pd(a5, b0, c5);
+        c6 = _mm512_fmadd_pd(a6, b0, c6);
+        c7 = _mm512_fmadd_pd(a7, b0, c7);
     }
-    _mm256_storeu_pd(&C(0, 0), c0);
-    _mm256_storeu_pd(&C(1, 0), c1);
-    _mm256_storeu_pd(&C(2, 0), c2);
-    _mm256_storeu_pd(&C(3, 0), c3);
-    _mm256_storeu_pd(&C(4, 0), c4);
-    _mm256_storeu_pd(&C(5, 0), c5);
-    _mm256_storeu_pd(&C(6, 0), c6);
-    _mm256_storeu_pd(&C(7, 0), c7);
+
+    _mm512_storeu_pd(&C(0, 0), c0);
+    _mm512_storeu_pd(&C(1, 0), c1);
+    _mm512_storeu_pd(&C(2, 0), c2);
+    _mm512_storeu_pd(&C(3, 0), c3);
+    _mm512_storeu_pd(&C(4, 0), c4);
+    _mm512_storeu_pd(&C(5, 0), c5);
+    _mm512_storeu_pd(&C(6, 0), c6);
+    _mm512_storeu_pd(&C(7, 0), c7);
 }
 
 void kernel_4x4_4x1(const int Ma, const int Mb, const int Mc, double *restrict a, double *restrict b, double *c)
@@ -1099,6 +1099,65 @@ void ear_kxk_kx4(
     }
 }
 
+void ear_4x8_8x4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        kernel_4x4_4x4(Ma, Mb, Mc, &A(0, i * 4), &B(i * 4, 0), c);
+    }
+}
+
+void ear_8x8_8x8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        kernel_8x8_8x8(Ma, Mb, Mc, a, &B(0, i * 8), &C(0, i * 8));
+    }
+}
+
+void ear_4x4_4x8(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        kernel_4x4_4x4(Ma, Mb, Mc, a, &B(0, i * 4), &C(0, i * 4));
+    }
+}
+
+void ear_8x4_4x4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        kernel_4x4_4x4(Ma, Mb, Mc, &A(i * 4, 0), b, &C(i * 4, 0));
+    }
+}
+
 // start of fields
 /*
     No need to provide information about matrix shape because we assume that
@@ -1132,6 +1191,95 @@ void field_kx4_4x4n(
     for (int i = 0; i < N; ++i)
     {
         ear_kx4_4x4(Ma, Mb, Mc, k, a, &B(0, i * 4), &C(0, i * 4));
+    }
+}
+
+void field_8x8_8x4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        ear_4x8_8x4(Ma, Mb, Mc, &A(i * 4, 0), b, &C(i * 4, 0));
+    }
+}
+
+void field_8x8n_8nx8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        ear_8x8_8x8n(Ma, Mb, Mc, N, &A(0, i * 8), &B(i * 8, 0), c);
+    }
+}
+
+void field_8x8n_8nx4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        field_8x8_8x4(Ma, Mb, Mc, &A(0, i * 8), &B(i * 8, 0), c);
+    }
+}
+
+void field_4x4_4x8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        ear_4x4_4x8(Ma, Mb, Mc, N, a, &B(0, i * 8), &C(0, i * 8));
+    }
+}
+
+void field_8x4_4x8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        field_4x4_4x8n(Ma, Mb, Mc, N, &A(i * 4, 0), b, &C(i * 4, 0));
+    }
+}
+
+void field_4x8_8x8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        field_4x4_4x8n(Ma, Mb, Mc, N, &A(0, i * 4), &B(i * 4, 0), c);
     }
 }
 
@@ -1275,6 +1423,96 @@ void farm_kxk_kxk(
     }
 }
 
+void farm_8nx8n_8nx4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        field_8x8n_8nx4(Ma, Mb, Mc, N, &A(i * 8, 0), b, &C(i * 8, 0));
+    }
+}
+
+void farm_8nx8n_8nx8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        field_8x8n_8nx8n(Ma, Mb, Mc, N, &A(i * 8, 0), b, &C(i * 8, 0));
+    }
+}
+
+void farm_8nx4_4x8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        field_8x4_4x8n(Ma, Mb, Mc, N, &A(i * 8, 0), b, &C(i * 8, 0));
+    }
+}
+
+void farm_8nx4_4x4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        ear_8x4_4x4(Ma, Mb, Mc, N, &A(i * 8, 0), b, &C(i * 8, 0));
+    }
+}
+
+void farm_4x8n_8nx8n(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        field_4x8_8x8n(Ma, Mb, Mc, N, &A(0, i * 8), &B(i * 8, 0), c);
+    }
+}
+
+void farm_4x8n_8nx4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        ear_4x8_8x4(Ma, Mb, Mc, &A(0, i * 8), &B(i * 8, 0), c);
+    }
+}
+
 // ranches start here
 
 // 4n x 4n multiplied by 4n x (4n + k)
@@ -1347,6 +1585,74 @@ void ranch_kxk_kx4nk(
 
     // k x k multiplied by k x k
     farm_kxk_kxk(Ma, Mb, Mc, k, a, &B(0, N * 4), &C(0, N * 4));
+}
+
+void ranch_8nx8n_8nx8n4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    const int plus4,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    // 8nx8n multiplied by 8nx8n
+    farm_8nx8n_8nx8n(Ma, Mb, Mc, N, a, b, c);
+
+    // 8nx8n multiplied by 8nx4
+    if (plus4 > 0)
+    {
+        farm_8nx8n_8nx4(Ma, Mb, Mc, N, a, &B(0, N * 8), &C(0, N * 8));
+    }
+}
+
+void ranch_8nx4_4x8n4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    // 8nx4 multiplied by 4x8n
+    farm_8nx4_4x8n(Ma, Mb, Mc, N, a, b, c);
+
+    // 8nx4 multiplied by 4x4
+    farm_8nx4_4x4(Ma, Mb, Mc, N, a, &B(0, N * 8), &C(0, N * 8));
+}
+
+void ranch_4x8n_8nx8n4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    // 4x8n multiplied by 8nx8n
+    farm_4x8n_8nx8n(Ma, Mb, Mc, N, a, b, c);
+
+    // 4x8n multiplied by 8nx4
+    farm_4x8n_8nx4(Ma, Mb, Mc, N, a, &B(0, N * 8), &C(0, N * 8));
+}
+
+void ranch_4x4_4x8n4(
+    const int Ma,
+    const int Mb,
+    const int Mc,
+    const int N,
+    double *restrict a,
+    double *restrict b,
+    double *c)
+{
+    // 4x4 multiplied by 4x8n
+    field_4x4_4x8n(Ma, Mb, Mc, N, a, b, c);
+
+    // 4x4 multiplied by 4x4
+    kernel_4x4_4x4(Ma, Mb, Mc, a, &B(0, N * 8), &C(0, N * 8));
 }
 
 const char *dgemm_desc = "My dgemm.";
